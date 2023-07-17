d7aae6ecae66be8db7054c97c536403622e39468
James Sharp <jsharp9012@gmail.com>
2023-07-17 21:52:20 +0000

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const admin = require('firebase-admin');
 const express = require('express');
+const bodyParser = require('body-parser'); // new line
 const app = express();
+
+// setup body-parser
+app.use(bodyParser.json()); // for parsing application/json
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -31,12 +35,46 @@ app.get("/recipes", async (req, res) => {
   res.json(recipeList);
 });
 
+app.post('/create-preferences', async (req, res) => {  // New block of code to handle preferences
+  let userPreferences = db.collection('preferences').doc(req.body.userId);
+
+  let setWithOptions = userPreferences.set({
+    vegan: req.body.vegan,
+    vegetarian: req.body.vegetarian,
+    glutenFree: req.body.glutenFree,
+    whole30: req.body.whole30,
+    pescetarian: req.body.pescetarian,
+    dislikedIngredients: req.body.dislikedIngredients,
+    dislikedCuisines: req.body.dislikedCuisines,
+    // add other preferences here
+  }, {merge: true});
+
+  res.send('Preferences created successfully');
+});
+
 app.get('/recipes/:id', async (req, res) => {
   // code to fetch a specific recipe from Firestore
 });
 
 app.get('/users/:id/preferences', async (req, res) => {
-  // code to fetch a user's preferences from Firestore
+  const { id } = req.params;
+  
+  console.log(`Received request for preferences of user: ${id}`);
+  
+  try {
+    const doc = await db.collection('Preferences').doc(id).get();
+    
+    if (!doc.exists) {
+      console.log('No such document!');
+      res.status(404).send('No such document!');
+    } else {
+      console.log('Document data:', doc.data());
+      res.json(doc.data());
+    }
+  } catch (error) {
+    console.error('Error getting document:', error);
+    res.status(500).send('Error getting document');
+  }
 });
 
 app.listen(3001, () => {
